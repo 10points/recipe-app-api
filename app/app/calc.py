837a537c75857561bc043411a93604fb2ac9681e
10points <75314303+10points@users.noreply.github.com>
2023-09-07 21:27:55 +0700

    Configuration Django

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calcultor fuctions
+"""
+
+def add(x, y):
+    """ add x and y and return result."""
+    return x+y
+
+def subtract(x, y):
+    """ subtrct x from y and return result."""
+    return y-x
